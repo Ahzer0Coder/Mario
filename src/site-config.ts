3dbf5180e39501d70ae02c80ec4b2d5602129123
ahmednagj100-sketch <openhands@all-hands.dev>
2026-08-01 01:42:13 +0300

    Initial commit for Mario project

---
+++ src/site-config.ts
@@ -0,0 +1,33 @@
+export const SITE = {
+  site: {
+    url: "https://gianmarcocavallo.com/",
+  },
+  author: {
+    firstName: "Ahmed",
+    lastName: "Shelbaya",
+    fullName: "Ahmed Shelbaya",
+    shortName: "Mario",
+    jobTitle: "Software engineering",
+  },
+  location: {
+    countryName: "Egypt",
+    countryCode: "EG",
+    timezone: "Africa/Cairo",
+  },
+  links: {
+    github: "https://github.com/Ahzer0Coder",
+    linkedin: "https://www.linkedin.com/in/ahmed-nagy-904683382/",
+    medium: "https://medium.com/@ahmednagj100",
+    discord: "https://discord.com/users/1406728434939465819",
+    email: "ahmednagj100@gmail.com",
+    my_website: " "
+  },
+  cal: {
+    username: "Ahmed",
+    durationNamespace: "15min",
+    origin: "",
+  },
+  visitedCountries: [
+    "Egypt"
+  ],
+};
